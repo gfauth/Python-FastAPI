e73dadec5476eb0637ba74a4634474f4e9e58051
Luan Fauth <luan.fauth@gmail.com>
2024-09-25 18:49:22 -0300

    Adicionando endpoint de retorno de html com teste meio tosco mas funcionado

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,8 +1,30 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_api.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olár mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    return """
+<html>
+    <head>
+    <style>
+        body { background-color: #1e211f; }
+        h1 { color: white }
+    </style>
+        <title>Nosso Olá mundo!</title>
+    </head>
+    <body>
+        <h1>Olá mundo!</h1>
+    </body>
+</html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_read_root_deve_retornar_ok_e_olar_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olár mundo!'}
+
+
+def test_read_html_deve_retornar_codigo_html_com_sucesso():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá mundo!</h1>' in response.text
